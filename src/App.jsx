904551b46c1bea180d9c222a1ad54d426208629a
Divyansh veer singh <veersinghdivyansh4089@gmail.com>
2026-07-05 00:55:21 +1000

    Added video stream playback to the selected frame slots - later to be replaced with the video capture from the webcam

--- src/App.jsx
+++ src/App.jsx
@@ -34,24 +34,6 @@ function App() {
   //   return () => cancelAnimationFrame(animationId)  // cleanup
   // }, [selectedFrame])
 
-  // if (!selectedFrame) {
-  //   return (
-  //     <div className="app">
-  //       <h1>Pick a frame</h1>
-  //       <div className="frame-picker">
-  //         {frameOptions.map((color) => (
-  //           <div
-  //             key={color}
-  //             className="frame-swatch"
-  //             style={{ backgroundColor: color }}
-  //             onClick={() => setSelectedFrame(color)}
-  //           />
-  //         ))}
-  //       </div>
-  //     </div>
-  //   )
-  // }
-
   return (
     <div className="app">
       <h1>Love booth </h1>
