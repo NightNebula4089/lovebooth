8d017da3b7abea444148117db3b49b7f814b0116
Divyansh veer singh <veersinghdivyansh4089@gmail.com>
2026-07-16 16:51:45 +1000

    Added turn servers

--- src/Firebase.js
+++ src/Firebase.js
@@ -17,6 +17,16 @@ const servers = {
     {
       urls : ["stun:stun.l.google.com:19302","stun:stun2.l.google.com:19302"]
     },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
   ],
   iceCandidatePoolSize : 10
 }
